0cb2e1cafb17dda7edce1cb8c18d001610191379
namddanh <namddanh@gmail.com>
2025-04-30 11:17:53 -0400

    bar complete

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
   import Question from "../components/Question.vue";
   import QuizHeader from "../components/QuizHeader.vue";
   import { useRoute } from "vue-router";
-  import { ref, watch } from "vue";
+  import { ref, watch, computed } from "vue";
   import quizes from "../data/quizes.json";
 
   const route = useRoute();
@@ -13,21 +13,32 @@
 
   const currentQuestionIndex = ref(0);
 
-  const questionStatus = ref(
-    `${currentQuestionIndex.value} / ${quiz.questions.length}`
-  );
+  // const questionStatus = ref(
+  //   `${currentQuestionIndex.value} / ${quiz.questions.length}`
+  // );
 
-  watch(
-    () => currentQuestionIndex.value,
-    () => {
-      questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
-    }
+  // watch(
+  //   () => currentQuestionIndex.value,
+  //   () => {
+  //     questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+  //   }
+  // );
+
+  const questionStatus = computed(() => {
+    return `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+  });
+
+  const barPercentage = computed(
+    () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
   );
 </script>
 
 <template>
   <div>
-    <QuizHeader :questionStatus="questionStatus" />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
