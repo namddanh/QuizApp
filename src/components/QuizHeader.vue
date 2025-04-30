107b7f6186a6a0856f8743991bbae6c23e45b853
namddanh <namddanh@gmail.com>
2025-04-30 10:58:57 -0400

    watch to implement the status bar added

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { defineProps } from "vue";
+
+  const { questionStatus } = defineProps(["questionStatus"]);
+</script>
+
+<template>
+  <header>
+    <h4>Question {{ questionStatus }}</h4>
+    <div class="bar">
+      <div class="completion"></div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    margin-top: 20px;
+  }
+
+  header h4 {
+    font-size: 40px;
+    color: black;
+  }
+
+  .bar {
+    width: 300px;
+    height: 50px;
+    border: 3px solid bisque;
+  }
+
+  .completion {
+    height: 100%;
+    width: 0%;
+    background-color: bisque;
+  }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,101 +1,36 @@
+<script setup>
+  import Question from "../components/Question.vue";
+  import QuizHeader from "../components/QuizHeader.vue";
+  import { useRoute } from "vue-router";
+  import { ref, watch } from "vue";
+  import quizes from "../data/quizes.json";
+
+  const route = useRoute();
+
+  const quizId = parseInt(route.params.id);
+
+  const quiz = quizes.find((q) => q.id === quizId);
+
+  const currentQuestionIndex = ref(0);
+
+  const questionStatus = ref(
+    `${currentQuestionIndex.value} / ${quiz.questions.length}`
+  );
+
+  watch(
+    () => currentQuestionIndex.value,
+    () => {
+      questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+    }
+  );
+</script>
+
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader :questionStatus="questionStatus" />
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the chemical value of table salt</h1>
-      </div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
-    <div class="options-container">
-      <div class="option">
-        <p class="option-label">A</p>
-        <div class="option-value">
-          <p>NaCl</p>
-        </div>
-      </div>
-      <div class="option">
-        <p class="option-label">B</p>
-        <div class="option-value">
-          <p>NaCl</p>
-        </div>
-      </div>
-      <div class="option">
-        <p class="option-label">C</p>
-        <div class="option-value">
-          <p>NaCl</p>
-        </div>
-      </div>
-      <div class="option">
-        <p class="option-label">D</p>
-        <div class="option-value">
-          <p>NaCl</p>
-        </div>
-      </div>
-    </div>
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
-
-<style scoped>
-  header {
-    margin-top: 20px;
-  }
-
-  header h4 {
-    font-size: 30px;
-    color: black;
-  }
-
-  .bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-  }
-
-  .completion {
-    height: 100%;
-    width: 0%;
-    background-color: bisque;
-  }
-
-  h1,
-  p {
-    color: black;
-  }
-
-  .question-container {
-    margin-top: 20px;
-  }
-
-  .question {
-    font-size: 40px;
-    margin-bottom: 20px;
-  }
-
-  .option {
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-  }
-
-  .option-label {
-    background-color: bisque;
-    width: 60px;
-    height: 60px;
-    font-size: 30px;
-    display: flex;
-    justify-content: center;
-    text-align: center;
-  }
-
-  .option-value {
-    background-color: rgb(244, 239, 239);
-    width: 100%;
-    font-size: 30px;
-    padding: 0 20px;
-  }
-</style>
